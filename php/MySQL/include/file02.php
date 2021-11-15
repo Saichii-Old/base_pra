d78d5b3e335dac624113babe44736444e6eca568
saichi <overy1399@gmail.com>
2021-11-15 09:30:42 +0800

    檔案拆分練習

---
+++ php/MySQL/include/file02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>include & require</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <!-- <header>11/15 檔案拆分</header> -->
+    <?php include "header.html"; ?>
+    <nav>
+        <a href="file01.php">file01.php</a>
+        <a href="file02.php">file02.php</a>
+        <a href="file03.php">file03.php</a>
+    </nav>
+    <div class="body">
+        file02 content
+    </div>
+    <footer>
+    110 php class
+    </footer>
+</body>
+</html>
