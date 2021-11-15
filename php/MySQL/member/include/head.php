0d50bfcb31fb0bbf76c811c77d4845d725c0be21
saichi <overy1399@gmail.com>
2021-11-15 11:59:14 +0800

    登入系統頁面

---
+++ php/MySQL/member/include/head.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="member_style.css">
---
+++ php/MySQL/member/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>channel</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+
+        }
+
+        nav {
+            height: 50px;
+            box-shadow: 0 2px 10px #ccc;
+            display: block;
+            width: 100%;
+            padding: 10px 5px;
+            text-align: right;
+        }
+        
+        button {
+            padding: 5px 10px;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <a href="reg.php"><button>註冊會員</button></a>
+        <a href="login.php"><button>登入</button></a>
+
+    </nav>
+    
+    <div>
+    <label for="" name="acc">account</label>    
+    <input type="text" name="acc" id="acc">
+
+    <label for="" name="ps">password</label>      
+    <input type="password" name="ps" id="ps">
+
+    </div>
+</body>
+</html>
