905fcf3198e223edd2e8c0315c27d080acbaf3e4
saichi <overy1399@gmail.com>
2021-11-15 14:10:07 +0800

    註冊表單

--- php/MySQL/member/reg.php
+++ php/MySQL/member/reg.php
@@ -14,7 +14,42 @@
     <?php include "./include/footer.php"; ?>
 
     <div class="content">
-register
+<form action="./api/reg.php" method="post" id="regForm">
+
+<table>
+    <tr>
+        <td>帳號:</td>
+        <td><input type="text" name="account" id=""></td>
+    </tr>
+    <tr>
+        <td>密碼:</td>
+        <td><input type="password" name="password" id=""></td>
+    </tr>
+    <tr>
+        <td>電子信箱:</td>
+        <td><input type="text" name="mail" id=""></td>
+    </tr>
+    <tr>
+        <td>姓名:</td>
+        <td><input type="text" name="name" id=""></td>
+    </tr>
+    <tr>
+        <td>聯絡電話:</td>
+        <td><input type="text" name="mobile" id=""></td>
+    </tr>
+    <tr>
+        <td>地址:</td>
+        <td><input type="text" name="address" id=""></td>
+    </tr>
+    <tr>
+        <td>生日:</td>
+        <td><input type="date" name="birthday" id=""></td>
+    </tr>
+</table>
+<div>
+<input type="submit" value="確認送出">
+</div>
+</form>
     </div>
 </body>
 </html>
