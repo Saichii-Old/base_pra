3f705b2bf904185895f16c3353bd82908720d6c8
Saichi <overy1399@gmail.com>
2021-11-22 10:22:12 +0800

    自訂函式練習

--- php/function/index.php
+++ php/function/index.php
@@ -1,18 +1,25 @@
 <?php
-
 add(3,5);
 echo "<br>";
-echo add(3,5)+10;
+echo add(3,5)+10; // add(3,5) 先產生出 echo $a + $b 的結果
 echo "<br>";
 $result = add(3,5);
 echo $result+10;
 echo "<br>";
 echo "result=" . $result;
 
+hello(" Nannan");
 
 function add($a,$b){
-    // echo $a + $b ;
-    return $a + $b;
+    echo $a + $b ;
+    return $a + $b; // 執行順序 echo $a + $b > 帶入 return 變數 > 進行運算
+}
+echo "<br>";
+// 寫出一個函式 可以輸入名字 echo "hello XXX"
+function hello ($name){
+    echo "hello" . $name ;
 }
 
+hello(" Nannan");
 ?>
+
