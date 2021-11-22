0830ad8ddbb2d8a2b3fd05e3dfc0bc9042577d68
Saichi <overy1399@gmail.com>
2021-11-22 10:33:42 +0800

    fuction 印星星

---
+++ php/function/star.php
@@ -0,0 +1,84 @@
+<?php
+//三角形星星函式
+for($i=5;$i<12;$i=$i+2){
+    stars($i);
+}
+
+function stars($row){
+    for($j=1;$j<=$row;$j++){
+        
+        for($i=0;$i<($row-$j);$i++){
+            echo "&ensp;";
+        }
+        
+        for($i=0;$i<(2*$j-1);$i++){
+           
+            echo "*";
+        }
+        echo "<br>";
+    }
+   
+}
+
+echo "<hr>"; //上下可以印出相同結果
+
+$row=5;
+for($j=1;$j<=$row;$j++){
+        
+    for($i=0;$i<($row-$j);$i++){
+        echo "&ensp;";
+    }
+    
+    for($i=0;$i<(2*$j-1);$i++){
+       
+        echo "*";
+    }
+    echo "<br>";
+}
+$row=7;
+for($j=1;$j<=$row;$j++){
+        
+    for($i=0;$i<($row-$j);$i++){
+        echo "&ensp;";
+    }
+    
+    for($i=0;$i<(2*$j-1);$i++){
+       
+        echo "*";
+    }
+    echo "<br>";
+}
+
+$row=9;
+for($j=1;$j<=$row;$j++){
+        
+    for($i=0;$i<($row-$j);$i++){
+        echo "&ensp;";
+    }
+    
+    for($i=0;$i<(2*$j-1);$i++){
+       
+        echo "*";
+    }
+    echo "<br>";
+}
+
+$row=11;
+for($j=1;$j<=$row;$j++){
+        
+    for($i=0;$i<($row-$j);$i++){
+        echo "&ensp;";
+    }
+    
+    for($i=0;$i<(2*$j-1);$i++){
+       
+        echo "*";
+    }
+    echo "<br>";
+}
+
+
+
+
+
+?>
