a222bda56e15a22f2a6e9c3e75adbcb49d47bb24
Saichi <overy1399@gmail.com>
2021-12-03 09:18:13 +0800

    新增編輯功能

---
+++ polling_hw/api/add.option.php
@@ -0,0 +1,15 @@
+<?php include_once "db.php";?>
+<?php
+
+$id=$_GET['id'];
+insert('options',['opt'=>"",'topic_id'=>$id]);
+
+to("../backend/?do=edit_subject&id=$id");
+?>
+
+
+
+
+
+
+
--- polling_hw/api/save_subject.php
+++ polling_hw/api/save_subject.php
@@ -6,15 +6,18 @@ include_once "db.php";
  * 
  */
 
-$topic_array = ['topic' => $_POST['subject']];
-insert('topics', $topic_array);
-$topic_sql = "select * from `topics` where `topic`='{$_POST['subject']}'";
+$topic_array=['topic'=>$_POST['subject']];
+insert('topics',$topic_array);
+$topic_sql="select * from `topics` where `topic`='{$_POST['subject']}'";
 echo $topic_sql;
-$topic = $pdo->query($topic_sql)->fetch();
+$topic=$pdo->query($topic_sql)->fetch();
 
 
-foreach ($_POST['options'] as $opt) {
-
-    $opt_array = ["opt" => $opt, "topic_id" => $topic['id']];
-    insert('options', $opt_array);
+foreach($_POST['options'] as $opt){
+    if($opt!=""){
+        $opt_array=["opt"=>$opt,"topic_id"=>$topic['id']];
+        insert('options',$opt_array);
+    }
 }
+
+?>
